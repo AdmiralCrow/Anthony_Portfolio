420ad874f95f3a03e1147d3c0ab3cb7062d8c1a5
AdmiralCrow <67215398+AdmiralCrow@users.noreply.github.com>
2023-01-11 14:57:47 -0500

    Update tip_calculator.py
    Created a Tip Calculator for the purpose of verifying best way to split bills amongst friends

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,11 +1,15 @@
 print("Welcome to the tip calculator!")
+
 #bill total
 bill = float(input("What was the total bill?\n $"))
+
 #tip amount
 tip = int(input("How much tip would you like to give? Exp. 10, 15?\n "))
+
 #How many peoply are you splitting the bill with?
 split = int(input("How many people to split the bill?\n "))
 
+#math
 tip_as_percent = tip/100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
